fa04c9f4f48cc64eca04db22966426b2d15796c7
apenfe <adrian1414@hotmail.es>
2024-02-11 22:08:27 +0100

    11-02-2024 REDES MONOCAPA perceptron and en MatLab

--- Java_eclipse/REDES_MONOCAPA/src/RED_NEURONAL_PERCEPTRON/Perceptron.java
+++ Java_eclipse/REDES_MONOCAPA/src/RED_NEURONAL_PERCEPTRON/Perceptron.java
@@ -1,7 +1,63 @@
 package RED_NEURONAL_PERCEPTRON;
 
-public class Perceptron{
-	
-	
-	
+import java.util.Arrays;
+
+public class Perceptron {
+
+	private double[] pesosSinapticos;
+	private double umbral;
+	private double costo;
+
+	public Perceptron(int entradas, double costo) {
+		this.pesosSinapticos = new double[entradas];
+		this.costo = costo;
+		this.umbral = Math.random()* 2 - 1;
+
+		for (int i = 0; i < entradas; i++) {
+			pesosSinapticos[i] = Math.random()* 2 - 1;
+		}
+		
+	}
+
+	public int feedForward(double[] entradas) {
+		
+		double neta = 0;
+		
+		for (int i = 0; i < entradas.length; i++) {
+			
+			neta += entradas[i] * pesosSinapticos[i] + umbral;
+			
+		}
+		
+		return activate(neta);
+	}
+
+	private int activate(double neta) {
+		
+		return (neta >= 0) ? 1 : -1;
+		
+	}
+
+	public void train(double[] inputs, int deseado) {
+		
+		int salida = feedForward(inputs);
+		
+		double error = deseado - salida;
+		
+		for (int i = 0; i < pesosSinapticos.length; i++) {
+			
+			pesosSinapticos[i] += error * inputs[i] * costo; // Aplicando la regla de actualización
+			
+		}
+
+		umbral += error * costo; // Actualización del umbral
+	}
+
+	public void printWeights() {
+		
+		System.out.println(Arrays.toString(pesosSinapticos));
+		System.out.println("Umbral: "+umbral);
+		
+	}
+
 }
---
+++ MATLAB/Redes monocapa/perceptron_and.m
@@ -0,0 +1,57 @@
+% Definicion de la funcion logica AND
+% X es la entrada de la red y D es la salida deseada
+
+X=[0 0 1 1;
+ 0 1 0 1];
+
+D=[0 0 0 1];
+
+% Grafica de estos patrones
+
+plotpv(X,D)
+
+% Generar perceptron con pesos fijos
+
+red=newp(X,D)
+red.iw{1,1}=randn(1,2);
+red.b{1}=randn(1,1);
+
+% visualizacion de la superficie inicial tras crear los primeros pesos
+
+Pesos=red.iw{1,1};
+Bias=red.b{1};
+plotpc(Pesos,Bias)
+
+% Entrenamiento de la red
+
+red=train(red,X,D)
+
+% Visualizacion de la superficie cuando ha sido entrenada
+
+figure;
+Pesos=red.iw{1,1};
+Bias=red.b{1};
+plotpc(Pesos,Bias)
+
+% Superponer los puntos en la misma figura
+hold on
+plot(X(1, D == 1), X(2, D == 1), 'ro'); % Puntos de clase 1 en rojo
+plot(X(1, D == 0), X(2, D == 0), 'bx'); % Puntos de clase 0 en azul
+hold off
+
+% Prueba de la salida de a red patron de prueba [0,0]
+
+in_prueba=[0;0];
+a=sim(red,in_prueba)
+
+% salida esperada a = 0
+
+% Prueba de la salida de a red patron de prueba [1,1]
+
+in_prueba=[1;1];
+a=sim(red,in_prueba)
+
+% salida esperada a = 0
+
+% Exportacion de la red Simulink
+gensim(red)
