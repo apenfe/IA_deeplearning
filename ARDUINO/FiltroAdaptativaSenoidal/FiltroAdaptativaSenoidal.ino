ed0cc0e31f6e33daba779057db19a9ab8eb653a3
apenfe <adrian1414@hotmail.es>
2024-02-13 19:27:45 +0100

    13-02-2024 ADALINE señal senoidal en Arduino

---
+++ ARDUINO/FiltroAdaptativaSenoidal/FiltroAdaptativaSenoidal.ino
@@ -0,0 +1,34 @@
+ #include <Math.h>
+
+ //Neuronas de entrada
+ const int N = 4;
+ //Neuronas de salida
+ const int M = 1;
+
+ const float pi = 3.1416;
+
+ // Definicion de muestras que se van a usar para el filtrado de la señal
+ // Son la entrada para la red neuronal
+
+ float X_K=0;
+ float X_K_1=0;
+ float X_K_2=0;
+ float X_K_3=0;
+
+ // Salida deseada
+
+ float D;
+
+ // Razon de aprendizaje
+
+ float alfa = 0.23;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
